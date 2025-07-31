7de1a1ddccb503dcc65f3d50dee0e7bbe0a7aec7
Purushottam Kumar Mandal <122455904+gitNinjaAtC@users.noreply.github.com>
2025-07-31 15:03:20 +0530

    Delete SearchFilter directory

--- SearchFilter/searchFilterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchBooks } = require("./searchFilterController");
-
-// Route for searching and filtering books
-router.get("/", searchBooks);
-
-module.exports = router;
